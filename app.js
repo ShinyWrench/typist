73a2286208fc388424e58d7fe22beee78cf0044d
Jason Peppers <jasonpeppers72@gmail.com>
2021-01-07 21:13:12 -0500

    move board stuff into a class

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let argv = require('minimist')(process.argv.slice(2));
 const { promisify } = require('util');
 const redis = require('redis');
 const express = require('express');
+const Board = require('./board');
 
 client = redis.createClient();
 
@@ -25,34 +26,5 @@ const constants = {
 //     console.log(`Listening on port ${port}`);
 // });
 
-function randomLetter() {
-    let randomNumber = 52 * Math.random();
-    let uppercase = Math.floor(randomNumber / 26) > 0;
-    return String.fromCharCode((uppercase ? 65 : 97) + (randomNumber % 26));
-}
-
-function printBoard(board) {
-    let output = '';
-    board.forEach((row) => {
-        output += '|';
-        row.forEach((letter) => {
-            output += `  ${letter}`;
-        });
-        output += '  |\n';
-        output += `|${' '.repeat(3 * row.length + 2)}|\n`;
-    });
-    console.log(output);
-}
-
-const boardWidth = 10;
-const boardLength = 35;
-let board = [];
-for (let iRow = 0; iRow < boardLength; iRow++) {
-    let row = [];
-    for (let iCol = 0; iCol < boardWidth; iCol++) {
-        row.push(randomLetter());
-    }
-    board.push(row);
-}
-
-printBoard(board);
+let board = new Board(35, 10);
+board.print();
