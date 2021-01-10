ad0e5e0d7625681232c0622a81f09ea60fd3edbf
Jason Peppers <jasonpeppers72@gmail.com>
2021-01-10 18:17:16 -0500

    remove console.log messages from client

--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,11 @@
 $(document).ready(() => {
     const webSocket = new WebSocket('ws://localhost:8080');
     let previousBoard = null;
+
     webSocket.onmessage = (message) => {
         if (previousBoard !== message.data) {
-            console.log(
-                `redraw\nprevious: ${previousBoard}\ncurrent: ${message.data}`
-            );
             $('#board').html(message.data);
             previousBoard = message.data;
-        } else {
-            console.log('no change');
         }
     };
 
